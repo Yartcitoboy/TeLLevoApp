7927591deadbe8128fc7d7bec4ddab2a12e8cf86
apeleoca <ca.apeleo@duocuc.cl>
2024-09-07 22:30:42 -0400

    creacion de recuperar contraseña

--- src/app/pages/resetpass/resetpass.page.ts
+++ src/app/pages/resetpass/resetpass.page.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { AlertController } from '@ionic/angular';
+import { UsuariosService } from 'src/app/services/usuarios.service';
 
 @Component({
   selector: 'app-resetpass',
@@ -6,10 +10,31 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./resetpass.page.scss'],
 })
 export class ResetpassPage implements OnInit {
+  alertButtons = ['Aceptar'];
 
-  constructor() { }
+  resetForm: FormGroup;
+  message = 'Elije qué tipo de usuario deseas ser en la aplicación.';
+
+  constructor(
+    private router: Router, 
+    private formBuilder: FormBuilder,
+    private alertController: AlertController,
+    private usuariosServices: UsuariosService
+  ) { 
+    this.resetForm = this.formBuilder.group({
+      email: ['', [Validators.required, Validators.email]],
+
+    });  
+  }
+
+  async Enviar(){
+
+    
+  }
 
   ngOnInit() {
+    
   }
+
 
 }
