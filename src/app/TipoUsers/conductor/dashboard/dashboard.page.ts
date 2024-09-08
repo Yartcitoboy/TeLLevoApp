e96596a11163d3fc1fc3ec8a76c157e63fdb2dfd
Is4 <is.riquelmet@duocuc.cl>
2024-09-07 23:20:38 -0400

    esta bueno

--- src/app/TipoUsers/conductor/dashboard/dashboard.page.ts
+++ src/app/TipoUsers/conductor/dashboard/dashboard.page.ts
@@ -9,7 +9,7 @@ import { person, time, qrCode } from 'ionicons/icons';
   styleUrls: ['./dashboard.page.scss'],
 })
 export class DashboardPage implements OnInit {
-
+//hola
   constructor(
     private menuController: MenuController
   ) {
