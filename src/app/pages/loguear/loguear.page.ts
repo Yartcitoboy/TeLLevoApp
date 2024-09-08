a6e937f6c1eaaafced6b7e7e5f814de36b0e9b55
alli-rubio <all.rubio@duocuc.cl>
2024-09-07 20:31:52 -0400

    probando cositas

--- src/app/pages/loguear/loguear.page.ts
+++ src/app/pages/loguear/loguear.page.ts
@@ -75,7 +75,7 @@ export class LoguearPage implements OnInit {
           this.router.navigate(['/usuarios']);
         } else if (usuario.tipo === 'pasajero') {
           this.router.navigate(['./pasajero-dashboard']);
-        } else if (usuario.tipo === 'pasajero') {
+        } else if (usuario.tipo === 'conductor') {
           this.router.navigate(['./conductor-dashboard']); 
         } else {
           this.router.navigate(['./invitado-dashboard']);
