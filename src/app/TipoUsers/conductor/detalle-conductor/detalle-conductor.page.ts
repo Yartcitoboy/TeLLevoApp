70b972c9cc59b3164530b02f3af0569593d6e951
Is4 <is.riquelmet@duocuc.cl>
2024-10-05 20:41:13 -0230

    service viaje

--- src/app/TipoUsers/conductor/detalle-conductor/detalle-conductor.page.ts
+++ src/app/TipoUsers/conductor/detalle-conductor/detalle-conductor.page.ts
@@ -12,6 +12,7 @@ export class DetalleConductorPage implements OnInit {
   direccionDestino: string = '';
   costo: string = '';
   cantidadPasajeros: string = '';
+  horario: string = ''; 
 
   constructor(
     private alertController: AlertController,
